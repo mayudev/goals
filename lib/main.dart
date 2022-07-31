a22e32b447ec6dafb9096bd7757b99aa926e72db
mayudev <99561108+mayudev@users.noreply.github.com>
2022-07-31 23:39:10 +0200

    feat: save theme preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:goals/pages/home.dart';
 import 'package:goals/theme.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,6 +20,8 @@ class GoalsApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    restoreTheme();
+
     return ValueListenableBuilder(
       valueListenable: _themeNotifier,
       child: HomePage(updateTheme: onThemeUpdate),
@@ -35,7 +38,20 @@ class GoalsApp extends StatelessWidget {
     );
   }
 
-  void onThemeUpdate(ThemeOption theme) {
+  Future<void> onThemeUpdate(ThemeOption theme) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString("theme", theme.name);
+
     _themeNotifier.value = theme;
+  }
+
+  Future<void> restoreTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    final theme = prefs.getString("theme");
+
+    if (theme == ThemeOption.dark.name) {
+      _themeNotifier.value = ThemeOption.dark;
+    }
   }
 }
